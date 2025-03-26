691188952da42711ac6bac6a36cbc60bc6b399dc
Rudransh Chouksey <choukseyrudransh22@gmail.com>
2025-03-27 01:41:32 +0530

    update

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -5,20 +5,38 @@
  * Compare it with the results from 3-promise-all.js
  */
 
-function wait1(t) {
-
+function wait1(time) {
+    return new Promise(resolve => setTimeout(resolve, time*1000));
 }
 
-function wait2(t) {
-
+// Placeholder wait function
+function wait2(time) {
+    return new Promise(resolve => setTimeout(resolve, time*1000));
 }
 
-function wait3(t) {
-
+// Placeholder wait function
+function wait3(time) {
+    return new Promise(resolve => setTimeout(resolve, time*1000));
 }
 
 function calculateTime(t1, t2, t3) {
+    let start = new Date();
 
+    return call(t1, t2, t3)
+        .then(function () {
+            let end = new Date();
+            return end.getTime() - start.getTime();
+        });
+}
+
+function call(t1, t2, t3) {
+    return wait1(t1)
+        .then(function () {
+            return wait2(t2);
+        })
+        .then(function () {
+            return wait3(t3);
+        });
 }
 
 module.exports = calculateTime;
